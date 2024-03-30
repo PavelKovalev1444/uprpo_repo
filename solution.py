98aef1a69fa64678c4947211aa4a6247bdd1c6fd
Pavel Kovalev <pavel.kovalev30@gmail.com>
2024-03-30 18:00:26 +0300

    UPRPO: fix fix stylings

--- solution.py
+++ solution.py
@@ -1,44 +1,56 @@
 import wikipedia
-def is_page_valid(page):
+
+
+def is_valid(page):
     try:
         wikipedia.page(page)
     except Exception:
         return False
     return True
-pagesAndlanguage = input().split(', ')
-#Point1
-def language_settle(pagesAndlanguage):
-    flag = 0                                                                    
-    if pagesAndlanguage[-1] in wikipedia.languages():                           
-        wikipedia.set_lang(pagesAndlanguage[-1])                                
-        flag=1                                                                  
-    else:                                                                       
-        print('no results')                                                     
+
+
+pages = input().split(', ')
+
+
+# Point1
+def language_settle(pages):
+    flag = 0
+    if pages[-1] in wikipedia.languages():
+        wikipedia.set_lang(pages[-1])
+        flag = 1
+    else:
+        print('no results')
     return flag
-#Point2
-if language_settle(pagesAndlanguage) == 1:
-    def max_words_and_title(pagesAndlanguage):
+
+
+# Point2
+if language_settle(pages) == 1:
+    def max_words_and_title(pages):
         maximum_words = 0
         maximum_title = ''
-        for i in range(len(pagesAndlanguage)-1):
-            words = wikipedia.page(pagesAndlanguage[i]).summary.split()
+        for i in range(len(pages)-1):
+            words = wikipedia.page(pages[i]).summary.split()
             words = len(words)
             if words > maximum_words:
                 maximum_words = words
-                maximum_title=wikipedia.page(pagesAndlanguage[i]).title
+                maximum_title = wikipedia.page(pages[i]).title
         return str(maximum_words)+' '+str(maximum_title)
-#Point3 
-    def output_spisok(pagesAndlanguage):
-        output = [pagesAndlanguage[0]]
-        for i in range(len(pagesAndlanguage)-1):
-            link = wikipedia.page(pagesAndlanguage[i]).links
-            if (str(pagesAndlanguage[i+1]) in link) and (pagesAndlanguage[i+1] != len(pagesAndlanguage)-1):
-                output.append(pagesAndlanguage[i+1])
-            else:
-                for j in link:
-                    if is_page_valid(j) and (pagesAndlanguage[i+1] in wikipedia.page(j).links):
-                        output.append(j)
-                        output.append(pagesAndlanguage[i+1])
-        return(output)
-    print(max_words_and_title(pagesAndlanguage))
-    print(output_spisok(pagesAndlanguage))
+
+
+# Point3
+def output_spisok(pages):
+    output = [pages[0]]
+    for i in range(len(pages)-1):
+        link = wikipedia.page(pages[i]).links
+        if (str(pages[i+1]) in link) and (pages[i+1] != len(pages)-1):
+            output.append(pages[i+1])
+        else:
+            for j in link:
+                if is_valid(j) and (pages[i+1] in wikipedia.page(j).links):
+                    output.append(j)
+                    output.append(pages[i+1])
+    return (output)
+
+
+print(max_words_and_title(pages))
+print(output_spisok(pages))
